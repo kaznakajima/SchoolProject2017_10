c5f786df5470bd7d0294db58d099ce16ee4d7f9e
なかジー <nkc.kazuya.nakajima@gmail.com>
2017-09-13 13:50:21 +0900

    プレイヤーの動き修正

--- Summer/Assets/Script/PlayerController.cs
+++ Summer/Assets/Script/PlayerController.cs
@@ -96,7 +96,7 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator LayerChange()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.75f);
 
         PlayerRen.sprite = PlayerSp[0];
 

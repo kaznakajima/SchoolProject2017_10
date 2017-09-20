fae53088121b28a2e07bbeda06b88f0ca55a8ca1
なかジー <nkc.kazuya.nakajima@gmail.com>
2017-09-20 09:59:03 +0900

    修正

--- Summer/Assets/Script/ObstacleController.cs
+++ Summer/Assets/Script/ObstacleController.cs
@@ -16,13 +16,6 @@ public class ObstacleController : MonoBehaviour
     // 障害物のスピード
     public float speed;
 
-    // PlayerControllerの参照
-    PlayerController _playerController;
-
-    // 衝突したオブジェクトのRigidbody
-    private Rigidbody2D PlayerRig;
-    // 衝突したオブジェクトのBoxCollider2D
-    private BoxCollider2D PlayerCollision;
     // 衝突エフェクト
     [SerializeField]
     SpriteRenderer ObstacleRen;
@@ -34,31 +27,53 @@ public class ObstacleController : MonoBehaviour
     public enum obstacleType
     {
         // 鳥
-        Leftbird,   
+        Leftbird,
         Rightbird,
         // 飛行機
         RightPlane,
-        LeftPlane
+        LeftPlane,
+        // 星
+        Star
 
     };
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        PlayerRig = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        PlayerCollision = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+        _mainCamera = Camera.main;
+
+        // 初期位置
+        switch (obstacle)
+        {
+
+            case obstacleType.Star:
+                transform.position = new Vector3(Random.Range(-2, 2), _mainCamera.transform.position.y + 5.5f, -1);
+                break;
+            case obstacleType.Leftbird:
+                transform.position = new Vector3(-3, Random.Range(_mainCamera.transform.position.y - 2, _mainCamera.transform.position.y + 2), -1);
+                break;
+            case obstacleType.LeftPlane:
+                transform.position = new Vector3(-3, Random.Range(_mainCamera.transform.position.y - 2, _mainCamera.transform.position.y + 2), -1);
+                break;
+            case obstacleType.Rightbird:
+                transform.position = new Vector3(3, Random.Range(_mainCamera.transform.position.y - 2, _mainCamera.transform.position.y + 2), -1);
+                break;
+            case obstacleType.RightPlane:
+                transform.position = new Vector3(3, Random.Range(_mainCamera.transform.position.y - 2, _mainCamera.transform.position.y + 2), -1);
+                break;
+        }
     }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
+
         switch (obstacle)
         {
             case obstacleType.Leftbird:
                 ObstaclePos.x = 1;
                 transform.position += ObstaclePos * speed * Time.deltaTime;
-                if(transform.position.x > getCameraRange_Right().x + 0.25f)
+                if (transform.position.x > getCameraRange_Right().x + 0.25f)
                 {
                     Destroy(gameObject);
                 }
@@ -73,7 +88,7 @@ public class ObstacleController : MonoBehaviour
                 break;
             case obstacleType.LeftPlane:
                 transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
-                ObstaclePos.x = 2;
+                ObstaclePos.x = 1;
                 transform.position += ObstaclePos * speed * Time.deltaTime;
                 if (transform.position.x > getCameraRange_Right().x + 1.0f)
                 {
@@ -81,56 +96,38 @@ public class ObstacleController : MonoBehaviour
                 }
                 break;
             case obstacleType.RightPlane:
-                ObstaclePos.x = -2;
+                ObstaclePos.x = -1;
                 transform.position += ObstaclePos * speed * Time.deltaTime;
                 if (transform.position.x < getCameraRange_Left().x - 1.0f)
                 {
                     Destroy(gameObject);
                 }
                 break;
+            case obstacleType.Star:
+                ObstaclePos.y = -2;
+                transform.position += ObstaclePos * speed * Time.deltaTime;
+                if (transform.position.y < getCameraRange_Right().y - 1.0f)
+                    Destroy(gameObject);
+                break;
         }
     }
 
     // 障害物にぶつかった時の処理
-    void OnTriggerEnter2D(Collider2D collision)
+    void PlayerHit()
     {
-        if (collision.gameObject.tag == "GameController")
-        {
-            //// 衝突した地点の座標を取得
-            //foreach (ContactPoint2D contact in collision.contacts)
-            //{
-            //    point = contact.point;
-            //    Debug.Log(point);
-            //}
 
-            _playerController.ObstacleHit();
-            ObstacleRen.sprite = HitEffect;
-            speed = 0;
-            Destroy(gameObject, 0.25f);
-
-            HitAction();
-        }
     }
 
-    void HitAction()
+    public void HitAction()
     {
-        if (ObstaclePos.x > 0)
-        {
-            PlayerRig.simulated = true;
-            PlayerRig.velocity = transform.right * 15.0f;
-            PlayerCollision.enabled = false;
-        }
-        if (ObstaclePos.x < 0)
-        {
-            PlayerRig.simulated = true;
-            PlayerRig.velocity = -transform.right * 15.0f;
-            PlayerCollision.enabled = false;
-        }
+        ObstacleRen.sprite = HitEffect;
+        speed = 0;
+        Destroy(gameObject, 0.25f);
     }
 
     private Vector3 getCameraRange_Left()
     {
-        _mainCamera = Camera.main;
+        //_mainCamera = Camera.main;
         Vector3 LeftRange = _mainCamera.ScreenToWorldPoint(Vector3.zero);
         // 上下反転させる
         LeftRange.Scale(new Vector3(1f, -1f, 1f));
@@ -140,8 +137,8 @@ public class ObstacleController : MonoBehaviour
 
     private Vector3 getCameraRange_Right()
     {
-        _mainCamera = Camera.main;
-        Vector3 RightRange = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0.0f));
+        //_mainCamera = Camera.main;
+        Vector3 RightRange = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
         // 上下反転させる
         RightRange.Scale(new Vector3(1f, -1f, 1f));
         RightRange.z = 0;

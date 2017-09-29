3a31557f8916a707f8d2488815097d4a75ab22fb
なかジー <nkc.kazuya.nakajima@gmail.com>
2017-09-29 14:10:17 +0900

    エフェクト実装

---
+++ Summer/Assets/Script/EffectController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject,1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+
+	}
+}
